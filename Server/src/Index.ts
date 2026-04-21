3d7ba25371a812951556ddd9262fff8a805051b2
ronaldSWE <ronaldokuk462@gmail.com>
2026-04-21 12:07:42 +0100

    InvoicePro

--- Server/src/Index.ts
+++ Server/src/Index.ts
@@ -14,7 +14,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", router);
+app.use("/api/invoices", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
