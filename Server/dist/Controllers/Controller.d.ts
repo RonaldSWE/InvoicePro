f80b647c810833a56127e4f5b6e8835c0de50a87
ronaldSWE <ronaldokuk462@gmail.com>
2026-04-21 15:51:53 +0100

    InvoicePro

--- Server/dist/Controllers/Controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Request, type Response } from "express";
-export declare function getInvoices(req: Request, res: Response): Promise<void>;
-export declare function getSingleInvoice(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function createInvoice(req: Request, res: Response): Promise<void>;
-export declare function updateInvoice(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function deleteInvoice(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=Controller.d.ts.map
