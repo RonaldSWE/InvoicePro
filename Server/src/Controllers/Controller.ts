a87611d46e1ab673f0081fe2b26fd6e688dd912c
ronaldSWE <ronaldokuk462@gmail.com>
2026-04-21 12:05:20 +0100

    InvoicePro

--- Server/src/Controllers/Controller.ts
+++ Server/src/Controllers/Controller.ts
@@ -30,6 +30,48 @@ export async function getSingleInvoice(req: Request, res: Response) {
   }
 }
 
-export async function createInvoice(req: Request, res: Response) {}
-export async function updateInvoice(req: Request, res: Response) {}
-export async function deleteInvoice(req: Request, res: Response) {}
+export async function createInvoice(req: Request, res: Response) {
+  try {
+    const { invoice } = req.body;
+    const newInvoice = new Invoice({ invoice });
+    await newInvoice.save(); // The save() add info to the DataBase
+    res.status(201).json(newInvoice);
+  } catch (error) {
+    console.log("Error while creating your invoice", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+}
+export async function updateInvoice(req: Request, res: Response) {
+  try {
+    const { invoice } = req.body;
+    const UpdatedInvoice = await Invoice.findByIdAndUpdate(req.params.id, {
+      invoice,
+    });
+
+    if (!UpdatedInvoice) {
+      return res.status(404).json({ msg: "Invoice not found" });
+    }
+
+    res.status(200).json(UpdatedInvoice);
+  } catch (error) {
+    console.log("Error while updating your invoice", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+}
+export async function deleteInvoice(req: Request, res: Response) {
+  try {
+    const { invoice } = req.body;
+    const DeletedInvoice = await Invoice.findByIdAndDelete(req.params.id, {
+      invoice,
+    });
+
+    if (!DeletedInvoice) {
+      return res.status(404).json({ msg: "Invoice not found" });
+    }
+
+    res.status(200).json(DeletedInvoice);
+  } catch (error) {
+    console.log("Error while updating your invoice", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+}
