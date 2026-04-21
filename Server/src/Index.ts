c815a6195ea298dd709028c38ef9eb8b6c2f55c7
ronaldSWE <ronaldokuk462@gmail.com>
2026-04-21 15:52:09 +0100

    InvoicePro

---
+++ Server/src/Index.ts
@@ -0,0 +1,22 @@
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./Routes/Routes.ts";
+import { connectDB } from "./Config/DB.ts";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/invoices", router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
